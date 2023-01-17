8e8b902024fffa6959fe7e57c10591a2456dfcc5
Yasin Jasinkiewicz Herrador <k.jasinkiewiczherrador@students.uu.nl>
2023-01-17 16:13:11 +0100

    Completed up to half of part 3

--- python_script.py
+++ python_script.py
@@ -60,3 +60,45 @@ results = {
 
 with open('results.json', 'w', encoding='utf-8') as file:
     json.dump(results, file, indent=4) 
+
+#Read codes from CSV file
+from csv import DictReader
+with open ('stations.csv') as file:
+    reader = DictReader(file)
+    stations_list = list(reader)
+
+results = []
+for station in stations_list:
+    #Get variables from csv file 
+    location = station['Location']
+    state = station['State']
+    station_id = station['Station']
+
+    total_monthly_precipitation = [0]*12
+    
+    for measurement in precipitation_data:
+        #Get the station's data
+        station_code = measurement['station']
+        if station_code == station_id:
+         #Calculate total monthly precipitation
+            date = measurement['date']
+            split_date = date.split('-') 
+            month = int(split_date[1])
+            total_monthly_precipitation[month-1] += measurement['value']
+        #Calculate yearly total
+            total_yearly_precipitation += measurement['value']
+            
+    partial_results = {
+        location:{
+            "station": station_id,
+            "state": state,
+            "total_monthly_precipitation": total_monthly_precipitation,
+            "total_yearly_precipitation": total_yearly_precipitation,
+            "relative_monthly_precipitation": [],
+            "relative_yearly_precipitation": [] }}
+    results.append(partial_results)
+    
+print(results)
+
+
+
